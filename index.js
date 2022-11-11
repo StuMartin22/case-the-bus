fcda6fa8dd72a788d21c763502a09c51429a2a86
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-11-11 12:30:12 -0600

    function to call for agencies and return just the agency names

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const transitAgencies = []
 const transitRoutes = []
 const agencyAPI = `https://svc.metrotransit.org/nextripv2/agencies`
+const routeAPI = `https://svc.metrotransit.org/nextripv2/routes`
 
 function getAgencies() {
     fetch(agencyAPI)
@@ -8,10 +9,10 @@ function getAgencies() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-        });
+            for (i = 0; i < data.length; i++) {
+                console.log(data[i].agency_name)
+            }})
 };
 
-console.log('test')
-
 getAgencies();
+
